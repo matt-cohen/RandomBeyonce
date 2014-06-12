d86b8fdfc561d37c6a45df08a80d70943413f717
jdmcpeek <david.mcpeek@yale.edu>
2014-06-12 11:06:39 -0400

    playlists work

--- beyonce.rb
+++ beyonce.rb
@@ -32,6 +32,40 @@ get '/sets' do
     erb :sets, :locals => {:list => session[:list]}
 end
 
+get '/sets/:setname/play' do
+    name = params[:setname]
+
+    array = session[:list][params[:setname]]["vidnums"]
+    @rest_of_vids = ''
+    session[:list][params[:setname]]["vidnums"].each do |vidnum|
+        if vidnum == session[:list][params[:setname]]["vidnums"][0]
+            @first_vid = vidnum
+        elsif vidnum != session[:list][params[:setname]]["vidnums"][session[:list][params[:setname]]["vidnums"].length-1]
+            @rest_of_vids << vidnum + ','
+        else
+            @rest_of_vids << vidnum
+        end
+    end
+
+    first = @first_vid
+    rest = @rest_of_vids
+
+    vid_playlist = '<iframe width="560" height="315" src="https://www.youtube.com/v/' + @first_vid + '?version=3&loop=1&playlist=' + @rest_of_vids + '" frameborder="0" allowfullscreen></iframe>'
+
+
+    #"<iframe width="560" height="315" src="https://www.youtube.com/v/first_vid.to_s?version=3&loop=1&playlist=rest_of_vids.to_s" frameborder="0" allowfullscreen></iframe>"
+# http://sitename.com/beyoncehits/play
+# http://sitename.com/summerbeyonce/play
+# http://sitename.com/winterbeyonce/play
+# http://sitename.com/2/play
+
+# ^ all of these map into this handler.  Based upon the set requested,
+# return the video player template with the correct video url's.
+
+
+    erb :display, :locals => {:name => name, :vid_playlist => vid_playlist, :first => first, :rest => rest}
+end
+
 get '/sets/new' do
     erb :new
 end
@@ -43,14 +77,16 @@ post '/sets' do
     erb :sets, :locals => {:list => session[:list]}
 end
 
-get '/sets/beyonce' do
-    set_name = params[:name]
-    session[:list][]
-    erb :display, :locals => {:set_name => set_name, :set_videos => set_videos}
-end
+# get '/sets/beyonce' do
+#     set_name = params[:name]
+#     session[:list][]
+#     erb :display, :locals => {:set_name => set_name, :set_videos => set_videos}
+# end
 
 get '/sets/beyonce/play' do
-    #play a specific set
+   current_video_list = session[:list][params[:button]]
+
+   erb :display
 end
 
 get '/sets/beyonce/edit' do
